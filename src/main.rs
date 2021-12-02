8ed60fcaa22fd49ddcf0805e684252df005194ba
Steven Lai <lai.steven@gmail.com>
2021-12-02 07:03:59 -0500

    day2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::str::FromStr;
 
 fn main() {
     let contents = fs::read_to_string("day1.txt").unwrap();
@@ -6,6 +7,12 @@ fn main() {
     let contents = contents.as_str();
     println!("Day 1a: {:?}", day1a(contents));
     println!("Day 1b: {:?}", day1b(contents));
+
+    let contents = fs::read_to_string("day2.txt").unwrap();
+
+    let contents = contents.as_str();
+    println!("Day2: {:?}", day2a(contents));
+    println!("Day2: {:?}", day2b(contents));
 }
 
 fn day1a(data: &str) -> usize {
@@ -29,6 +36,71 @@ fn day1b(data: &str) -> usize {
         .count()
 }
 
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "forward" => Ok(Direction::Forward),
+            "down" => Ok(Direction::Down),
+            "up" => Ok(Direction::Up),
+            _ => Err(()),
+        }
+    }
+}
+
+fn day2a(data: &str) -> u32 {
+    let depths = data
+        .lines()
+        .map(|m| m.split(" ").collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+
+    let mut horizontal: u32 = 0;
+    let mut depth: u32 = 0;
+
+    for m in depths {
+        let val: u32 = m[1].parse::<u32>().unwrap();
+        match Direction::from_str(m[0]).unwrap() {
+            Direction::Forward => horizontal = horizontal + val,
+            Direction::Down => depth = depth + val,
+            Direction::Up => depth = depth - val,
+        }
+    }
+
+    horizontal * depth
+}
+
+fn day2b(data: &str) -> u32 {
+    let depths = data
+        .lines()
+        .map(|m| m.split(" ").collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+
+    let mut horizontal: u32 = 0;
+    let mut depth: u32 = 0;
+    let mut aim: u32 = 0;
+
+    for m in depths {
+        let val: u32 = m[1].parse::<u32>().unwrap();
+        match Direction::from_str(m[0]).unwrap() {
+            Direction::Forward => {
+                horizontal = horizontal + val;
+                depth = depth + (val * aim);
+            }
+            Direction::Down => aim = aim + val,
+            Direction::Up => aim = aim - val,
+        }
+    }
+
+    horizontal * depth
+}
+
 #[test]
 fn test_day1() {
     let input = "199
@@ -44,4 +116,17 @@ fn test_day1() {
 
     assert_eq!(day1a(input), 7);
     assert_eq!(day1b(input), 5);
+}
+
+#[test]
+fn test_day2() {
+    let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+    assert_eq!(day2a(input), 150);
+    assert_eq!(day2b(input), 900);
 }
